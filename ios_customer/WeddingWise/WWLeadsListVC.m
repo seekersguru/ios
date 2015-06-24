de5e3ad7da207a19f44ca232de6123b52ad7f671
iOSDeepak <deepakglbitm07@gmail.com>
2015-06-24 23:34:58 +0530

    Leads

---
+++ ios_customer/WeddingWise/WWLeadsListVC.m
@@ -0,0 +1,37 @@
+//
+//  WWLeadsListVC.m
+//  WeddingWise
+//
+//  Created by Deepak Sharma on 6/24/15.
+//  Copyright (c) 2015 DS. All rights reserved.
+//
+
+#import "WWLeadsListVC.h"
+
+@interface WWLeadsListVC ()
+
+@end
+
+@implementation WWLeadsListVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
