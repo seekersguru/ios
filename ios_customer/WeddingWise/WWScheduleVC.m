d302175744b212c51cd73f2eaf447f53acc22d59
Shiv Vaishnav <shiv.vaishnav@ranosys.com>
2015-07-12 23:21:45 +0530

    updating schedule visit page with new date picker.

--- ios_customer/WeddingWise/WWScheduleVC.m
+++ ios_customer/WeddingWise/WWScheduleVC.m
@@ -22,13 +22,27 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated{
+    _scheduleTimeLabel.hidden = YES;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)scheduleVisitAction:(id)sender {
+    _scheduleTimeLabel.hidden = NO;
     //call api here
+    
+    NSDate *selectedDate = [_datePicker date];
+    NSDate *selectedTime = [_timePicker date];
 
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"dd/MM/yyyy"];
+    NSString *date = [dateFormatter stringFromDate:selectedDate];
+    [dateFormatter setDateFormat:@"hh:mm a"];
+    NSString *time = [dateFormatter stringFromDate:selectedTime];
+    
+    [_scheduleTimeLabel setText:[NSString stringWithFormat:@"Visit scheduled at %@ %@",date,time]];
 }
 @end
